06d9ff166154144280ea4f101579e5b6b33ab8ae
whwwhwjddn123 <whwhwjddn123@gmail.com>
2025-06-20 14:39:07 +0900

    board detail save update

--- src/main/java/com/tenco/blog/board/BoardController.java
+++ src/main/java/com/tenco/blog/board/BoardController.java
@@ -1,7 +1,9 @@
 package com.tenco.blog.board;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardPersistRepository boardPersistRepository;
+
+    @GetMapping("/board/save-form")
+    public String saveForm(){
+        return "board/save-form";
+    }
+
+    @PostMapping("/board/save")
+    public String saveForm(BoardRequest.SaveDTO reqDTO){
+        // HTTP 요청 본문 : title=값&content=값&username=값
+        // form 태그의 MIME 타입 ( application/x-www-form-urlencoded )
+
+        // Board board = new Board(reqDTO.getTitle(),reqDTO.getContent(),reqDTO.getUsername());
+        Board board = reqDTO.toEntity();
+        boardPersistRepository.save(board);
+
+        return "redirect:/";
+    }
+
+    @GetMapping({"/","/index"})
+    public String index(Model model){
+        List<Board> boards = boardPersistRepository.findAll();
+        model.addAttribute("boardList",boards);
+        return "index";
+    }
+    
+    // 게시글 상세보기
+    // 주소설계
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable(name = "id")Long id,HttpServletRequest request){
+        Board board = boardPersistRepository.findById(id);
+        request.setAttribute("board",board);
+        return "board/detail";
+    }
 
 }
